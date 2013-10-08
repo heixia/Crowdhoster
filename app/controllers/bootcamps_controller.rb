6b776619b86f707da0568422a208b96c3ce6f423
oak86 <email@omarkhafagy.com>
2013-10-08 15:52:49 -0400

    adding create action to bootcamps controller

--- app/controllers/bootcamps_controller.rb
+++ app/controllers/bootcamps_controller.rb
@@ -1,18 +1,27 @@
 class BootcampsController < ApplicationController
 
   def index 
-    @bootcamps=Bootcamp.all
+    @bootcamps = Bootcamp.all
   end
 
   def show
-    @bootcamp=Bootcamp.find(params[:id])
+    @bootcamp = Bootcamp.find(params[:id])
   end
 
   def new
-    @bootcamp=Bootcamp.new
+    @bootcamp = Bootcamp.new
+  end
+
+  def create
+    @bootcamp = Bootcamp.new(params[:bootcamp])
+    if @bootcamp.save
+      redirect_to @bootcamp, :notice => "Thanks for adding your bootcamp!"
+    else
+      render :action => 'new', :notice => "Oops! That didn't work."
   end
 
   def edit
+    @bootcamp = Bootcamp.find(params[:id])
   end
 
   def update
