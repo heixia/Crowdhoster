f792c92cb7d4d23978d8ac25c4b6fd2bef8ded8d
Eric Stiens <estiens@server.fake>
2013-10-12 15:37:12 -0400

    styling bootcamp list and adding location filters to model

--- app/models/bootcamp.rb
+++ app/models/bootcamp.rb
@@ -8,7 +8,70 @@ extend FriendlyId
 friendly_id :name , :use => :slugged
 
 acts_as_taggable
-acts_as_taggable_on :types
+acts_as_taggable_on :tags
+
+def self.canada
+  where(:country => "Canada")
+end
+
+def self.united_states
+  where(:country => %W(USA #{"United States"} US))
+end
+
+def self.ontario
+  if self.canada
+    where(:st_pr => %W(ON #{"Ontario"}))
+  end
+end
+
+def self.british_columbia
+  if self.canada
+    where(:st_pr => %W(BC #{"British Columbia"}))
+  end
+end
+
+def self.west_coast
+  if self.united_states
+    where(:st_pr => %W(CA OR WA))
+  end
+end
+
+def self.east_coast
+  if self.united_states
+    where(:st_pr => %W(NY ME CT NJ VT MA PA NH RI))
+  end
+end
+
+def self.mountain
+  if self.united_states
+    where(:st_pr => %W(ID WY UT NV CO MT))
+  end
+end
+
+def self.southwest
+  if self.united_states
+    where(:st_pr => %W(AZ NM OK TX))
+  end
+end
+
+def self.south
+  if self.united_states
+    where(:st_pr => %W(WV MD AR TN NC LA MS SC AL FL GA KY DE))
+  end
+end
+
+def self.midwest
+    if self.united_states
+      where(:st_pr => %W(OH MO AZ IA WI MN KS NE IL IN MN ND SD))
+    end
+end
+
+def self.rest_of_world
+  if !self.united_states && !self.canada
+  end
+end
+
+
 
 
 
